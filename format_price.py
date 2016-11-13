70b59cfcf3396ae9459bf7cb4a6b29436cf83506
ram0973 <ram0973@gmail.com>
2016-11-14 01:13:27 +0300

    начальный коммит

--- format_price.py
+++ format_price.py
@@ -1,5 +1,43 @@
-def format_price(price):
-    pass
+# -*- coding: utf-8 -*-
+import argparse
+
+import sys
+
+import decimal
+from decimal import Decimal
+
+
+def format_price(price: str) -> str:
+    """
+    :param price: из базы данных приходит строка вида 3245.000000
+    :return: cтрока вида 3 245 или None
+    """
+    try:
+        return ' '.join('{:,}'.format(int(Decimal(price))).split(','))
+    except (ValueError, decimal.InvalidOperation):
+        return None
+
+
+def enable_win_unicode_console():
+    """
+    Включаем правильное отображение unicode в консоли под MS Windows
+    """
+    if sys.platform == 'win32':
+        import win_unicode_console
+        win_unicode_console.enable()
+
+
+def main():
+    enable_win_unicode_console()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--string', '--s',
+                        help='Введите строку вида 3245.000000',
+                        required=True)
+
+    string = parser.parse_args().string
+    print('Результат: ', format_price(string))
+
 
 if __name__ == '__main__':
-    pass
+    main()
--- tests.py
+++ tests.py
@@ -1,1 +1,57 @@
-# TODO place your tests here
+import unittest
+
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_price_is_numeric(self):
+        price = format_price('12O.3')
+        self.assertEqual(price, None)
+
+    def test_price_correctly_cleared_from_spaces_or_tnr(self):
+        price = format_price(' \t\n\r  \t   234.454555   \n  \r   \t\n\r ')
+        self.assertEqual(price, '234')
+
+    def test_price_have_space_delimiters(self):
+        price = format_price('123456789.123456 ')
+        self.assertEqual(price, '123 456 789')
+
+    def test_price_truncates_fractional_part(self):
+        price = format_price('123.123456 ')
+        self.assertEqual(price, '123')
+
+    def test_price_keeps_minus_sign(self):
+        price = format_price('-123456.123456 ')
+        self.assertEqual(price, '-123 456')
+
+    def test_price_ignoring_plus_sign(self):
+        price = format_price('+123456.123456 ')
+        self.assertEqual(price, '123 456')
+
+    def test_price_can_be_negative_and_have_no_integer_part(self):
+        price = format_price('-.123456 ')
+        self.assertEqual(price, '0')
+
+    def test_price_can_be_positive_and_have_no_integer_part(self):
+        price = format_price('+.123456 ')
+        self.assertEqual(price, '0')
+
+    def test_price_can_have_no_fractional_part(self):
+        price = format_price('-123456789.')
+        self.assertEqual(price, '-123 456 789')
+
+    def test_price_can_have_no_dot(self):
+        price = format_price('-123456789')
+        self.assertEqual(price, '-123 456 789')
+
+    def test_price_is_not_rounding(self):
+        price = format_price('1.999999')
+        self.assertEqual(price, '1')
+
+    def test_price_can_be_huge(self):
+        price = format_price('999999999999999999999999999999999')
+        self.assertEqual(price, '999 999 999 999 999 999 999 999 999 999 999')
+
+if __name__ == '__main__':
+    unittest.main()
+
